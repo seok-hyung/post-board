dbd2c5b1465f3c387d4997e0a22b03e3521d5233
seok-hyung <korea970223@naver.com>
2024-03-21 20:58:41 +0900

    :gear:Chore : topLevelAwait 설정

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,18 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+// const nextConfig = {
+//   experimental: {
+//     appDir: true,
+//   },
+//   webpack(config) {
+//     config.experiments = { ...config.experiments, topLevelAwait: true };
+//     return config;
+//   },
+// };
+// module.exports = nextConfig;
+
+// webpack(){} 부분만 붙여넣으면 될텐데 근데 nodejs 버전이 낮으면 못쓰는 기능이라
+// 나중에 클라우드로 서버 배포할 때 클라우드서비스에서 nodejs 높은버전 제공을 안해주면 못씁니다.
+// 그래서 지금은 일반적으로는 쓰지 않습니다.
