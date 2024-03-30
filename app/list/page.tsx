c13176fe4b2d042d0793259bd694849432105f1b
seok-hyung <korea970223@naver.com>
2024-03-30 23:37:14 +0900

    :sparkles:Feat :  ListItem 컴포넌트화 및 ObjectId 타입에러 해결

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,41 +1,25 @@
-'use client';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { connectDB } from '@/util/database';
+import ListItem from './ListItem';
 
-const List = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const db = (await connectDB).db('board');
-      const result = await db.collection('post').find().toArray();
-      // MongoDB 문서를 Post 타입으로 변환
-      const postsData: Post[] = result.map((doc) => ({
-        _id: doc._id,
-        title: doc.title,
-        content: doc.content,
-      }));
-      setPosts(postsData);
-    };
-    fetchData();
-  }, []); // 빈 의존성 배열을 추가하여 컴포넌트가 마운트될 때 한 번만 실행되도록 함
+const List = async () => {
+  const db = (await connectDB).db('board');
+  let result = await db.collection('post').find().toArray();
+  const posts: Post[] = result.map((doc) => ({
+    _id: doc._id.toString(), // _id를 문자열로 변환
+    title: doc.title,
+    content: doc.content,
+  }));
   return (
-    <>
-      {posts.map((data, index) => (
-        <div key={index} className="list-bg">
-          <div className="list-item">
-            <h4>{data.title}</h4>
-            <p>{data.content}</p>
-          </div>
-        </div>
-      ))}
-    </>
+    <div className="list-bg">
+      <ListItem result={posts} />
+    </div>
   );
 };
 
 export default List;
-
 interface Post {
-  title: string;
   content: string;
+  _id: any;
+  title: string;
 }
