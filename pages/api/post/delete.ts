d0da259df5d9d0fff2d10b425b8eb9473a4089f4
seok-hyung <korea970223@naver.com>
2024-04-03 22:43:25 +0900

    :fire:Delete : 불필요한 코드 삭제

--- pages/api/post/delete.ts
+++ pages/api/post/delete.ts
@@ -1,15 +1,11 @@
-import { ObjectId } from 'mongodb';
 import { connectDB } from '@/util/database';
+import { ObjectId } from 'mongodb';
 
 const handler = async (req: any, res: any) => {
-  if (req.method === 'POST') {
+  if (req.method === 'DELETE') {
     const db = (await connectDB).db('board');
-    await db.collection('post').deleteOne(req.body);
-
-    // 정상적으로 문서가 업데이트된 후 리디렉션
+    await db.collection('post').deleteOne({ _id: new ObjectId(req.body.postId) });
     res.status(200).json({ message: 'Post successfully deleted' });
-    // res.status(300).redirect('/list'); //일반적으로, 리디렉션은 HTTP 응답 코드 3xx를 사용하여 처리됩니다.
-    console.log('delete.ts 핸들러 정상 작동');
   } else {
     // POST 요청이 아닐 경우 에러 처리
     res.status(405).end(); // Method Not Allowed
