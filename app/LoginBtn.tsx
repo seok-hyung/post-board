26527a9ac2e051719c424a9d343f3bfaab51a431
seok-hyung <korea970223@naver.com>
2024-04-05 16:44:58 +0900

    :sparkles:Feat : next-auth sighIn() 로그인 버튼 구현

---
+++ app/LoginBtn.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { signIn } from 'next-auth/react';
+
+const LoginBtn = () => {
+  return (
+    <button
+      onClick={() => {
+        signIn();
+      }}
+    >
+      로그인
+    </button>
+  );
+};
+export default LoginBtn;
