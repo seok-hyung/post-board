a259a19a4626e8e464798e113e8addbcb1dc88cd
seok-hyung <korea970223@naver.com>
2024-04-12 21:16:20 +0900

    :saprkles:Feat : 회원가입 API 핸들러 작성

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -5,10 +5,10 @@ import bcrypt from 'bcrypt';
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     let hash = await bcrypt.hash(req.body.password, 10);
-    console.log(hash);
-    console.log(req.body);
+    req.body.password = hash;
     let db = (await connectDB).db('board');
     await db.collection('user_cred').insertOne(req.body);
+    res.status(200).json('회원가입 성공');
   }
 };
 export default handler;
