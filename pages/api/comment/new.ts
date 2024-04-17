07f14c52f113831c98ece9cecac3f612f5512cc6
seok-hyung <korea970223@naver.com>
2024-04-17 19:06:58 +0900

    :sparkles:Feat : 댓글API 구현

---
+++ pages/api/comment/new.ts
@@ -0,0 +1,31 @@
+import { connectDB } from '@/util/database';
+import { ObjectId } from 'mongodb';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
+type sessionType = {
+  user: {
+    name: string;
+    email: string;
+  };
+  expires: string;
+};
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const commentReq = JSON.parse(req.body);
+  //   console.log(commentReq);
+  const session = await getServerSession(req, res, authOptions);
+
+  if (req.method === 'POST' && session) {
+    let commentObj = {
+      content: commentReq.comment,
+      author: session.user.email,
+      parent: new ObjectId(commentReq._id),
+    };
+
+    const db = (await connectDB).db('board');
+    let result = await db.collection('comment').insertOne(commentObj);
+    res.status(200).json('저장완료');
+  }
+};
+
+export default handler;
