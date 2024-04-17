7df3067f2c826816ba65b998f956fa05b3c22aea
seok-hyung <korea970223@naver.com>
2024-04-17 19:07:36 +0900

    :sparkles:Feat : Comment 컴포넌트에 props 전달

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -1,10 +1,18 @@
 import { connectDB } from '@/util/database';
 import { ObjectId } from 'mongodb';
+import Comment from './Comment';
 
-interface DetailProps {
+type DetailProps = {
   params: {
     id: string;
   };
+};
+
+interface Post {
+  _id: ObjectId;
+  title: string;
+  content: string;
+  author: string;
 }
 
 const Detail = async (props: DetailProps) => {
@@ -17,6 +25,7 @@ const Detail = async (props: DetailProps) => {
       <h4>상세페이지</h4>
       <h4>{result.title}</h4>
       <p>{result.content}</p>
+      <Comment _id={result._id.toString()} author={result.author} />
     </div>
   );
 };
