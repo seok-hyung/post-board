3dc48fd1f5e0a9aba770bd07f347635399b593cb
seok-hyung <korea970223@naver.com>
2024-04-21 21:19:16 +0900

    :sparkles:Feat : 작성한 댓글 보이게끔 수정

--- app/detail/[id]/Comment.tsx
+++ app/detail/[id]/Comment.tsx
@@ -1,15 +1,38 @@
 'use client';
 
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, useEffect } from 'react';
 type CommentProps = {
   _id: string;
   author: string;
 };
+type DataProps = {
+  author: string;
+  content: string;
+  parent: string;
+  _id: string;
+};
 const Comment = ({ _id, author }: CommentProps) => {
   const [comment, setComment] = useState('');
+  const [datas, setDatas] = useState([]);
+
+  // 아래 html 보여준 후, 그 이후 실행됨
+  useEffect(() => {
+    fetch(`/api/comment/list?id=${_id}`)
+      .then((r) => r.json())
+      .then((result) => {
+        setDatas(result);
+      });
+  }, [datas]);
+
   return (
     <>
+      <hr />
       <div>댓글목록</div>
+      {datas.length > 0
+        ? datas.map((data: DataProps, index) => {
+            return <p key={index}>{data.content}</p>;
+          })
+        : '로딩중'}
       <input
         type="text"
         onChange={(e: ChangeEvent<HTMLInputElement>) => {
